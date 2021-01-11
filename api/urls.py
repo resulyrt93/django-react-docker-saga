3119b592efd7893f45b19e81a19ea3c4fa581c0e
resulyrt93 <resulyrt93@gmail.com>
2021-01-12 00:04:07 +0300

    static fix

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
@@ -10,4 +11,7 @@ urlpatterns = [
 
 urlpatterns += static(settings.ASSETS_URL, document_root=settings.ASSETS_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+
+
 
