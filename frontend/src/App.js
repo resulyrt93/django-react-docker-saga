8eb43eaf96fdf8e047a70db79d8e9ed5794e7a81
resulyrt93 <resulyrt93@gmail.com>
2021-01-11 21:57:41 +0300

    #main page edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/resulyrt93"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Resul Yurttakalan
         </a>
       </header>
     </div>
